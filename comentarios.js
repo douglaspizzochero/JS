fed9dfc7c0b622f07025336fab9e4060ea4a3b9b
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-08 23:14:50 -0300

    first commit

---
+++ comentarios.js
@@ -0,0 +1,9 @@
+// Aqui nós temos comentários em linha
+
+/* Comentários em blocos
+    Diversas linha
+    uaaaaaaaaaaaaaaaaaau
+    Esse código abaixo escreve algo no Devtools
+*/
+console.log ("Olá, tudo bem?")
+console.log ("Me llamo Douglas, y no tu")
