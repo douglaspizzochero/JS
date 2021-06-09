f5562e1cd490a5418c9593098e4af7377b99043f
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-08 23:31:36 -0300

    adicionei um cosole.log

--- comentarios.js
+++ comentarios.js
@@ -1,9 +1,10 @@
 // Aqui nós temos comentários em linha
 
 /* Comentários em blocos
-    Diversas linha
+    Diversas linhas
     uaaaaaaaaaaaaaaaaaau
     Esse código abaixo escreve algo no Devtools
 */
 console.log ("Olá, tudo bem?")
 console.log ("Me llamo Douglas, y no tu")
+console.log ("that's cool")
