b7aa538e262504df4943b6028d33e27ca9b5b5f5
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-15 15:42:35 -0300

    Utilizando Array

---
+++ atividade_praticando_e_avançando.js
@@ -0,0 +1,75 @@
+// Atividade
+
+//1 let weight
+
+//2 console.log(typeof weight)
+
+//3 
+/* 
+let nome = "Douglas"
+let idade = 24
+let stars = 5.2
+let isSubscribed = true
+
+let apelido, age, numeroQuebrado, inscrito
+apelido = "Douglas"
+age = 24
+numeroQuebrado = 2.4
+inscrito = false */
+
+//4 Qual tipo de dado é o student?
+/*let student = {
+    nome: "Douglas",
+    idade: 24,
+    stars: 5.2,
+    isSubscribed: true
+};
+console.log(typeof student) //object
+console.log (student)
+*/
+
+//5 Mostre no console a seguinte mensagem:
+//<name> de idade <agr> pesa <weight> kg.
+
+/*let student = {
+   nome: "Douglas",
+    idade: 24,
+    stars: 5.2,
+    isSubscribed: true,
+    weight: 77,*/
+//}
+//console.log(`O ${student.nome} tem ${student.idade} anos e ${student.weight} kg.`);
+
+//6 Declare uma variável do tipo Array de nome students e atribua a ele nenhum valor ou 
+//somente um array vazio
+
+//let students = []
+
+//students = [
+//    student
+//]
+
+//console.log(students)
+//console.log(students[0])
+//console.log(student.lenght) //pq deu errado?*/
+
+//7
+
+/*const douglas = {
+    nome: "Douglas",
+    age: 24,
+    weight: 78,
+    isSubscribed: true
+}
+
+students = [
+    student,
+    douglas,
+]
+console.log (students)
+ 
+//9
+console.log(a)
+var a = 1
+
+//hosting - undefined */
