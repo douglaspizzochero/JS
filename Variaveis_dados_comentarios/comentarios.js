a4156c28186c6f67220e60c352cd7fe830016f78
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-16 10:17:44 -0300

    como fazer comentários

---
+++ Variaveis_dados_comentarios/comentarios.js
@@ -0,0 +1,11 @@
+// Aqui nós temos comentários em linha
+
+/* Comentários em blocos
+    Diversas linhas
+    uaaaaaaaaaaaaaaaaaau
+    Esse código abaixo escreve algo no Devtools
+*/
+console.log ("Olá, tudo bem?")
+console.log ("Me llamo Douglas, y no tu")
+console.log ("that's cool")
+//fim
