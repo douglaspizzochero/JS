4879e7bb661fa8d3868e1262776bbf13a3c00109
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-16 10:15:47 -0300

    function e entendimento

---
+++ functions/functions_2.js
@@ -0,0 +1,17 @@
+console.log('Aula sobre Functions');
+// criar um aplicativo de frases motivacionai
+
+// declaration - declaração da função
+//  function statement 
+function creatPhrases () {
+    console.log ("Você consegue")
+    console.log ("Acredite no processo")
+    console.log ("Eu sou o Douglas")
+    }
+//
+creatPhrases()
+creatPhrases()
+// executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+console.log ("Fim do código")
