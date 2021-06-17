048370f26080d578b6caaf82ddb652419e2b594c
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-17 09:08:41 -0300

    Funções => Arrow

---
+++ functions/functions_arrow.js
@@ -0,0 +1,10 @@
+// arrow function
+
+const sayMyName = (nome) =>/*arrow*/ {
+    console.log (nome)
+}
+
+sayMyName ("Douglas")
+
+// É uma forma diferente de "escrever function", mas não tem só isso,
+//Aguardar resposta da diferença
