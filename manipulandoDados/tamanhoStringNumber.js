5dad067243c13fe2e2b5f0c60f843c64296ea700
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-18 11:26:54 -0300

    tamanho da palavra e do numero

---
+++ manipulandoDados/tamanhoStringNumber.js
@@ -0,0 +1,6 @@
+//Faremos agora para saber quantos caracteres tem uma palavra
+//Também podemos contar quantos números tem o "number"
+let palavra = "Inconstitucionalissimamente"
+console.log (palavra.length)
+let numero = 15975368422486
+console.log (String(numero).length)
