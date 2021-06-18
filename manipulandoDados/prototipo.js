40e25d147f54a037901bf2307603f086c1ffcd62
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-18 11:26:04 -0300

    protótipo

---
+++ manipulandoDados/prototipo.js
@@ -0,0 +1,18 @@
+/* Prototype
+
+prototype-based language
+prototype chain
+__proto__
+
+É comum dizer que o JS é uma liguagem baseada em protótipo
+Quando eu crio um objeto ele vai, automaticamente, reverenciar outro objeto criado previamente
+vulgo protótipo. Nesse protótipo há um monte de funcionalidades e propriedades
+
+Experimente, por exemplo, colocar no Devtools {} e clicar Enter
+Para acessar prototipos de numero coloque no DevTools: 23.0.__proto__
+De nome "nome".__proto__
+Boolean: true.__proto__
+
+
+*/
+
