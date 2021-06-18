545fd8178a8fa2e2fc41bf08a5053a526a11b81a
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-18 11:40:52 -0300

    String Number

--- manipulandoDados/tamanhoStringNumber.js
+++ manipulandoDados/tamanhoStringNumber.js
@@ -2,5 +2,8 @@
 //Também podemos contar quantos números tem o "number"
 let palavra = "Inconstitucionalissimamente"
 console.log (palavra.length)
+
 let numero = 15975368422486
 console.log (String(numero).length)
+//Para contarmos a quantidade de números temos que tranformá-los
+//em String
