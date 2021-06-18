7bbaaec9be259a681fc7cb7663fe67fd9f13a97e
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-18 11:22:17 -0300

    string em number

---
+++ manipulandoDados/typeConversion_typeCoersion.js
@@ -0,0 +1,20 @@
+/*
+Type Conversion (capacidade nossa de causar a conversão(typecasting) - Quando eu altero, explicitamente, um dado de um tipo para outro
+x
+Type coersion(coersitivamente/forçar a troca) - É o JS, implicitamente, fazendo a troca, de maneira forçosa, podendo usar a Type conversion
+
+- Alteração de um tipo de dado para outro tipo
+*/
+console.log("Exemplo de Type Coersion: "); "\n"
+console.log('10' + 16);
+
+// É como se o JS estivesse fazendo o seguinte procedimento: console.log('10'+'16')
+// Transformou o "number" em "string"
+
+console.log("Exemplo de Type Coversion: "); "\n"
+console.log(Number('10')+16);
+
+//Alteramos manualmente alteramos o tipo, colocamos o "Number"
+
+
+ 
