1417b485c1850a68ad0236d6dd5f32b63eb6e02a
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-18 11:24:35 -0300

    String em numero

---
+++ manipulandoDados/number_string.js
@@ -0,0 +1,9 @@
+// Transformar String em Número e Número em String
+let texto = "123"
+console.log(Number(texto))
+// Acima transformamos string em número
+let numero = 123 
+console.log(String(numero))
+//Acima transformamos número em string
+console.log(typeof texto)
+console.log(typeof numero)
