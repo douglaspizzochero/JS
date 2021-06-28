e464aa7ba84ab5a4182af83d6302c268f631e818
Douglas Pizzochero <douglaspizzochero@hotmail.com>
2021-06-28 13:07:48 -0300

    operadoresAritiméticosExemplos

---
+++ Expressões_e_Operadores/operadoresAritiméticos.js
@@ -0,0 +1,47 @@
+/*  Operadores Aritiméticos */
+
+//Multiplicação
+console.log (10*5)
+console.log(2.9*1.4)
+
+//Divisão
+console.log(52/2)
+
+//Soma
+console.log(10+2)
+
+//Subtração
+console.log (29-10)
+
+//Resto da divisão % -> O resto da div por outro é? e ele dará a resposta
+console.log (11 % 2)
+//ou
+let remainder
+remainder = (11 % 3)
+console.log(remainder)
+
+//Incremento - Increment - Significa adicionar mais um valor e tem como símbolo o
+//"++"
+
+let increment = 0
+//increment ++
+//console.log (++increment) - incremento antes
+console.log (increment++) //incremento depois
+//console.log (++increment)
+console.log(increment)
+
+//Decrement
+let decrement = 0
+//console.log(decrement--)
+console.log(--decrement)
+
+//Exponencial **
+console.log (2**3)
+
+//Grouping operator ( )
+//Operador que agrupa expressoões, o parenteses 
+let total = 2 + 3 * 5
+console.log(total)
+let total1 = (2+3) * 5
+console.log (total1)
+
